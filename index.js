c75d7a0829c834279c2f2d17f3119a6e6b3cb0d6
antonioomodho <antonioomodho@gmail.com>
2022-11-29 09:51:02 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+const companyName = 'Scuber'
+let mostProfitableNeighborhood = 'Chelsea'
+let companyCeo = ('Susan Smith')
+
+it('is set as Scuber', function () {
+    expect(companyName).to.equal('Scuber');
+  });
